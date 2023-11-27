402d2053db8f086c95a5dbd4fb1eeb976ee0f926
Rasel <mdraselkibria11@gmail.com>
2023-11-27 20:52:16 +0600

    testimonialSlide redesign

--- src/components/TestimonialSlide.jsx
+++ src/components/TestimonialSlide.jsx
@@ -1,147 +1,113 @@
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/free-mode';
-import 'swiper/css/pagination';
-import { FreeMode,Autoplay } from 'swiper/modules';
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
+import { FreeMode, Autoplay } from "swiper/modules";
 const TestimonialSlide = () => {
-    const breakpoints = {
-        // when window width is >= 390px
-        390: {
-            slidesPerView: 1,
-            spaceBetween: 10,
-        },
-        // when window width is >= 768px
-        768: {
-            slidesPerView: 2,
-            spaceBetween: 20,
-        },
-        // when window width is >= 1200px
-        1200: {
-            slidesPerView: 3,
-            spaceBetween: 30,
-        },
-    };
+  const breakpoints = {
+    // when window width is >= 390px
+    390: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    // when window width is >= 768px
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    // when window width is >= 1200px
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+  };
+  const review = [
+    {
+      id: 1,
+      image: "https://tecdn.b-cdn.net/img/Photos/Avatars/img%20%2810%29.jpg",
+      name: "Anna Smith",
+      comments:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id quam sapiente molestiae numquam quas, voluptates omnis nulla eaodio quia similique corrupti magnam.",
+    },
+    {
+      id: 2,
+      image: "https://tecdn.b-cdn.net/img/Photos/Avatars/img%20%2810%29.jpg",
+      name: "Anna Smith",
+      comments:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id quam sapiente molestiae numquam quas, voluptates omnis nulla eaodio quia similique corrupti magnam.",
+    },
+    {
+      id: 3,
+      image: "https://tecdn.b-cdn.net/img/Photos/Avatars/img%20%2810%29.jpg",
+      name: "Anna Smith",
+      comments:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id quam sapiente molestiae numquam quas, voluptates omnis nulla eaodio quia similique corrupti magnam.",
+    },
+    {
+      id: 4,
+      image: "https://tecdn.b-cdn.net/img/Photos/Avatars/img%20%2810%29.jpg",
+      name: "Anna Smith",
+      comments:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id quam sapiente molestiae numquam quas, voluptates omnis nulla eaodio quia similique corrupti magnam.",
+    },
+    {
+      id: 5,
+      image: "https://tecdn.b-cdn.net/img/Photos/Avatars/img%20%2810%29.jpg",
+      name: "Anna Smith",
+      comments:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id quam sapiente molestiae numquam quas, voluptates omnis nulla eaodio quia similique corrupti magnam.",
+    },
+  ];
 
-    return (
-        <div className="p-10 bg-slate-200">
-            <div className="text-center">
-                <h1 className="text-3xl font-bold">
-                     <span className="text-success">Testimonials</span>
-                </h1>
-                <p className="font-bold">What our student say about us</p>
+  return (
+    <div className="p-16 bg-slate-50">
+      <div className="text-center pb-3 md:pb-10">
+        <h1 className="text-3xl font-bold">
+          <span className="text-success">Testimonials</span>
+        </h1>
+        <p className="font-bold">What our student say about us</p>
+      </div>
+      <Swiper
+        slidesPerView={2}
+        spaceBetween={30}
+        breakpoints={breakpoints}
+        loop={true}
+        autoplay={{
+          delay: 4000,
+          disableOnInteraction: false,
+        }}
+        speed={600}
+        freeMode={true}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[FreeMode, Autoplay]}
+      >
+        {review.map((item, i) => (
+          <SwiperSlide>
+            <div className="block rounded-lg bg-white p-8">
+              <div className="">
+                <div className="flex justify-center">
+                  <img
+                    src={item.image}
+                    className="rounded-full border-2"
+                    width={70}
+                    alt={item.name}
+                  />
+                </div>
+                <p className="my-2 text-xl text-center font-semibold">
+                  {item.name}
+                </p>
+              </div>
+              <div>
+                <p className="text-center">{item.comments}</p>
+              </div>
             </div>
-            <Swiper
-                slidesPerView={3}
-                spaceBetween={30}
-                breakpoints={breakpoints}
-                loop={true}
-                autoplay={{
-                    delay: 2500,
-                    disableOnInteraction: false,
-                }}
-                freeMode={true}
-                pagination={{
-                    clickable: true,
-                }}
-                modules={[FreeMode,Autoplay]}
-            >
-                <SwiperSlide>
-                    <div className="m-4 block rounded-lg bg-white p-6 shadow">
-                        <div className="">
-                            <div className=" flex items-center justify-center gap-5 md:mx-0 md:w-96 lg:mb-0">
-                                <img src="https://tecdn.b-cdn.net/img/Photos/Avatars/img%20%2810%29.jpg" className="rounded-full shadow-md" width={50} alt="" />
-                                <p className="mb-2 text-xl font-semibold">
-                                    Anna Smith
-                                </p>
-                            </div>
-                            <div className="md:ml-6">
-                                <p className="my-6">
-                                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id
-                                    quam sapiente molestiae numquam quas, voluptates omnis nulla
-                                    ea odio quia similique corrupti magnam.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <div className="m-4 block rounded-lg bg-white p-6 shadow">
-                        <div className="">
-                            <div className=" flex items-center justify-center gap-5 md:mx-0 md:w-96 lg:mb-0">
-                                <img src="https://tecdn.b-cdn.net/img/Photos/Avatars/img%20%2810%29.jpg" className="rounded-full shadow-md" width={50} alt="" />
-                                <p className="mb-2 text-xl font-semibold">
-                                    Anna Smith
-                                </p>
-                            </div>
-                            <div className="md:ml-6">
-                                <p className="my-6">
-                                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id
-                                    quam sapiente molestiae numquam quas, voluptates omnis nulla
-                                    ea odio quia similique corrupti magnam.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <div className="m-4 block rounded-lg bg-white p-6 shadow">
-                        <div className="">
-                            <div className=" flex items-center justify-center gap-5 md:mx-0 md:w-96 lg:mb-0">
-                                <img src="https://tecdn.b-cdn.net/img/Photos/Avatars/img%20%2810%29.jpg" className="rounded-full shadow-md" width={50} alt="" />
-                                <p className="mb-2 text-xl font-semibold">
-                                    Anna Smith
-                                </p>
-                            </div>
-                            <div className="md:ml-6">
-                                <p className="my-6">
-                                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id
-                                    quam sapiente molestiae numquam quas, voluptates omnis nulla
-                                    ea odio quia similique corrupti magnam.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <div className="m-4 block rounded-lg bg-white p-6 shadow">
-                        <div className="">
-                            <div className=" flex items-center justify-center gap-5 md:mx-0 md:w-96 lg:mb-0">
-                                <img src="https://tecdn.b-cdn.net/img/Photos/Avatars/img%20%2810%29.jpg" className="rounded-full shadow-md" width={50} alt="" />
-                                <p className="mb-2 text-xl font-semibold">
-                                    Anna Smith
-                                </p>
-                            </div>
-                            <div className="md:ml-6">
-                                <p className="my-6">
-                                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id
-                                    quam sapiente molestiae numquam quas, voluptates omnis nulla
-                                    ea odio quia similique corrupti magnam.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <div className="m-4 block rounded-lg bg-white p-6 shadow">
-                        <div className="">
-                            <div className=" flex items-center justify-center gap-5 md:mx-0 md:w-96 lg:mb-0">
-                                <img src="https://tecdn.b-cdn.net/img/Photos/Avatars/img%20%2810%29.jpg" className="rounded-full shadow-md" width={50} alt="" />
-                                <p className="mb-2 text-xl font-semibold">
-                                    Anna Smith
-                                </p>
-                            </div>
-                            <div className="md:ml-6">
-                                <p className="my-6">
-                                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id
-                                    quam sapiente molestiae numquam quas, voluptates omnis nulla
-                                    ea odio quia similique corrupti magnam.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </SwiperSlide>
-            </Swiper>
-        </div>
-    )}
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+};
 
 export default TestimonialSlide;
