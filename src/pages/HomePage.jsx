1efddbe85c0bd7305403991c54e2995182aa540f
Sabbir Hossain <sabbir15-2809@diu.edu.bd>
2023-11-30 20:49:02 +0600

    [Edujar-LMS]: Code Refactoring and New Features Implementation

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,8 @@
 import Categories from "../components/Categories.jsx";
 import Course from "../components/course/Course.jsx";
 import Landing from "../components/Landing.jsx";
+import SectionLast from "../components/SectionLast.jsx";
+import TestimonialSlide from "../components/TestimonialSlide.jsx";
 import WhyLearn from "../components/WhyLearn.jsx";
 import MainLayout from "../layout/MainLayout.jsx";
 
@@ -11,6 +13,8 @@ const HomePage = () => {
       <Course />
       <Categories />
       <WhyLearn />
+      <SectionLast />
+      <TestimonialSlide />
     </MainLayout>
   );
 };
