dfd09db9540b0cf9ce5ff04f6f4e15048426fe24
Rasel <mdraselkibria11@gmail.com>
2023-12-04 18:19:41 +0600

    blog page complete

--- src/components/blog/BlogSidebar.jsx
+++ src/components/blog/BlogSidebar.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+import image from "../../assets/about-banner.jpg";
 
 const BlogSidebar = () => {
-  return <div>sidebar</div>;
+  return (
+    <div>
+      <div className="bg-white p-3 grid grid-cols-3 gap-2 mb-3 rounded-md">
+        <img src={image} alt="" />
+        <span className="col-span-2">
+          Related post title Related post titleRelated post title
+        </span>
+      </div>
+    </div>
+  );
 };
 
 export default BlogSidebar;
