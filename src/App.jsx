82620f6ffeb6ba8a783014cecc6932d755ceff8c
Sabbir Hossain <sabbir15-2809@diu.edu.bd>
2023-12-13 00:43:50 +0600

    [Blog, Notification]: Features Implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import NotFound from "./components/common/NotFound.jsx";
+import AllCourses from "./components/course/AllCourses.jsx";
 import BlogDetailsPage from "./pages/BlogDetailsPage.jsx";
 import CategoryCoursePage from "./pages/CategoryCoursePage.jsx";
 import CourseDetailsPage from "./pages/CourseDetailsPage.jsx";
@@ -20,10 +21,10 @@ const App = () => {
         <Route path="/forget-password" element={<ForgetPasswordPage />} />
         <Route path="/my-courses" element={<LessonModulePage />} />
         <Route path="/profile" element={<ProfilePage />} />
-        <Route path="*" element={<NotFound />} />
         <Route path="/course-details/:id" element={<CourseDetailsPage />} />
         <Route path="/course-by-category/:id" element={<CategoryCoursePage />} />
-        <Route path="/blog-details" element={<BlogDetailsPage />} />
+        <Route path="/all-courses" element={<AllCourses />} />
+        <Route path="/blogs/:id" element={<BlogDetailsPage />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
