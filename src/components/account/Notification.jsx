aa7211e0ba090e27d8ffe6dc9fc29bb9a36b4f8c
Rasel <mdraselkibria11@gmail.com>
2023-12-17 18:36:21 +0600

    Notification section complete and add a model name=NotificationModel

--- src/components/account/Notification.jsx
+++ src/components/account/Notification.jsx
@@ -1,9 +1,13 @@
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
 import { GET_ALL_NOTIFICATION_REQUEST } from "../../apiRequest/API";
+import NotificationModel from "./NotificationModel";
 
 const Notification = () => {
   const [notification, setNotification] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+  const onClick = () => {
+    setShowModal(!showModal);
+  };
   useEffect(() => {
     (async () => {
       let result = await GET_ALL_NOTIFICATION_REQUEST();
@@ -13,45 +17,7 @@ const Notification = () => {
   return (
     <div className="container mx-auto px-4 sm:px-8">
       <div className="py-8">
-        <div>
-          <h2 className="text-2xl font-semibold leading-tight">Users</h2>
-        </div>
-
         <div className="my-2 flex sm:flex-row flex-col">
-          <div className="flex flex-row mb-1 sm:mb-0">
-            <div className="relative">
-              <select className="h-full rounded-l border block appearance-none w-full bg-white border-gray-400 text-gray-700 py-2 px-4 pr-8 leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
-                <option>5</option>
-                <option>10</option>
-                <option>20</option>
-              </select>
-              <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                <svg
-                  className="fill-current h-4 w-4"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                >
-                  <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-                </svg>
-              </div>
-            </div>
-            <div className="relative">
-              <select className=" h-full rounded-r border-t sm:rounded-r-none sm:border-r-0 border-r border-b block appearance-none w-full bg-white border-gray-400 text-gray-700 py-2 px-4 pr-8 leading-tight focus:outline-none focus:border-l focus:border-r focus:bg-white focus:border-gray-500">
-                <option>All</option>
-                <option>Active</option>
-                <option>Inactive</option>
-              </select>
-              <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                <svg
-                  className="fill-current h-4 w-4"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                >
-                  <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-                </svg>
-              </div>
-            </div>
-          </div>
           <div className="block relative">
             <span className="h-full absolute inset-y-0 left-0 flex items-center pl-2">
               <svg
@@ -109,13 +75,10 @@ const Notification = () => {
                       </p>
                     </td>
                     <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                      <span className="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight">
-                        <span
-                          aria-hidden
-                          className="absolute inset-0 bg-green-200 opacity-50 rounded-full"
-                        />
-                        <button className="relative">Explore</button>
-                      </span>
+                      <NotificationModel
+                        title={item["title"]}
+                        message={item["message"]}
+                      />
                     </td>
                   </tr>
                 ))}
